db344e72d965e28be89b34d126c381495a774f87
Josephus Paye II <j.paye96@gmail.com>
2020-03-03 22:09:46 +1100

    Add emojicon

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import 'focus-visible';
+import * as emojicon from 'emojicon';
 import Vue from 'vue';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+
+emojicon.set('🔎');
 
 new Vue({
   render: h => h(App),
